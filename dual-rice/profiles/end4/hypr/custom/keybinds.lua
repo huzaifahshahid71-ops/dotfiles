f5aef6f706c448668597841d595a51764dd22e8d
Huzaifah Shahid <huzaifahshahid71@gmail.com>
2026-09-15 21:18:30 +0300

    Upgrade Obsidian to latest end4-pC

--- dual-rice/profiles/end4/hypr/custom/keybinds.lua
+++ dual-rice/profiles/end4/hypr/custom/keybinds.lua
@@ -1,11 +1,11 @@
 hl.bind("CTRL+SUPER+ALT+Slash", hl.dsp.exec_cmd("xdg-open ~/.config/hypr/custom/keybinds.lua"), {description = "Edit user keybinds"} )
 
--- HUZAIFAH-DESKTOP-SWITCHER
+-- HUZAIFAH-MULTI-RICE-SWITCHER
 hl.unbind("SUPER + SHIFT + D")
 hl.bind(
     "SUPER + SHIFT + D",
-    hl.dsp.exec_cmd(os.getenv("HOME") .. "/.local/bin/desktop-switch"),
-    { description = "Desktop: Switch profile" }
+    hl.dsp.exec_cmd("qs -c multi-rice-switcher"),
+    { description = "Desktop: Open Multi-Rice switcher" }
 )
 
 -- HUZAIFAH-REFRESH-SWITCHER
